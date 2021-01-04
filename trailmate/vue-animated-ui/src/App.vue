747dcb94e1afae8e9a2caa75e700cc3573f97cf8
Merve Aydin <merveaydin.ieee@gmail.com>
2021-01-04 20:46:45 +0300

    bg color favicon and logo changed

--- trailmate/vue-animated-ui/src/App.vue
+++ trailmate/vue-animated-ui/src/App.vue
@@ -3,9 +3,6 @@
     <!-- Navigation -->
     <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
       <div class="container">
-        <a class="navbar-brand float-left" href="" target="_blank">
-           positronX.io
-        </a>
         <ul class="nav navbar-nav flex-row float-right">
           <li class="nav-item">
             <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
